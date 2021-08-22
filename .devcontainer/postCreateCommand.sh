8a54fcc7a1919e4bcd65c6e07c38b48c16ba1a81
Jean-Paul van Ravensberg <14926452+DevSecNinja@users.noreply.github.com>
2021-08-22 09:05:16 +0000

    Add C sample code

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 # Set specific Git settings for container
-if [[ $(git config --worktree --get commit.gpgsign) == true ]]; then
+if [[ $(git config --get commit.gpgsign) == true ]]; then
    git config --worktree commit.gpgsign false
 else
    echo "Git config is not using gpgsign, proceeding..."
 fi
+
+# Run sample script
+echo "Running sample script..."
+gcc ./sample/hello-world.c -o ./sample/hello-world.out
+./sample/hello-world.out
