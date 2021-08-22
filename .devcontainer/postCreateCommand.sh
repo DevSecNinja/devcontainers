e81911f3cb7ccad30a218789de8e9aa551ec47bc
Jean-Paul van Ravensberg <14926452+DevSecNinja@users.noreply.github.com>
2021-08-22 12:33:45 +0000

    Set default formatter

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -5,6 +5,7 @@ set -e
 
 # Set specific Git settings for container
 if [[ $(git config --get commit.gpgsign) == true ]]; then
+   echo "Disabling gpgsign on Git worktree level"
    git config --worktree commit.gpgsign false
 else
    echo "Git config is not using gpgsign, proceeding..."
